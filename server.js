0b4aa04512d29095928c0736bacdf6f616c27e4d
György Csadó <gyorgy.csado@gmail.com>
2016-02-01 13:15:35 +0100

    add server

---
+++ server.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+app.use(logRequest);
+app.use(express.static("public"));
+app.use(bodyParser.json());
+
+var port = parseInt(process.env.PORT || "3000")
+app.listen(port, function () {
+  console.log("Listening on ", port, )
+});
+
+
+function logRequest(req, res, next) {
+  var parts = [
+    new Date(),
+    req.method,
+    req.originalUrl,
+  ];
