c36a7d6a4f0819b7afd5bbe2fc0134cfdfc082e4
Daniella Korossy <koda5055@gmail.com>
2016-02-01 14:53:44 +0100

    fixes minor style elements

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
+'use strict';
+
 var gulp = require('gulp');
-var gulpUtil = require("gulp-util");
+var gulpUtil = require('gulp-util');
 
 gulp.task('default', function() {
-  return gulpUtil.log("gulp is ready");
+  return gulpUtil.log('gulp is ready');
 });
 
 var jshint = require('gulp-jshint');
