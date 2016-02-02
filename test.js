91da70e2a34ebbe88dff883d2e6f6f608be00a1d
Daniel Bodonyi <bodonyid@gmail.com>
2016-02-02 10:37:45 +0100

    Write test tests for GET and server response

--- test.js
+++ test.js
@@ -1,7 +1,23 @@
 'use strict';
 
-describe('A suite', function() {
-  it('contains spec with an expectation', function() {
-    expect(true).toBe(true);
+var request = require('request');
+
+var baseUrl = 'http://epam-2-calorie-app.herokuapp.com/';
+
+describe('Calorie Counter App Server', function() {
+  describe('GET /', function() {
+    it('returns status code 200', function(done) {
+      request.get(baseUrl, function(error, response, body) {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+    });
+
+    it('returns Hello World', function(done) {
+      request.get(baseUrl, function(error, response, body) {
+        expect(body).toBe('Hello World');
+        done();
+      });
+    });
   });
 });
