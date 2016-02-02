cfa79199adb12382156469882bd5546124c419a3
György Csadó <gyorgy.csado@gmail.com>
2016-02-02 15:02:42 +0100

    fix server listening

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
 'use strict';
 
 var express = require('express');
+var bodyParser = require('body-parser');
+var functions = require('./functions.js');
+
 var app = express();
 
+var port = parseInt(process.env.PORT || '3000');
+app.listen(port, function () {
+  console.log('Listening on port', port);
+});
 
-function serverStarting() {
-  var port = parseInt(process.env.PORT || '3000');
-  app.listen(port, function () {
-    console.log('Listening on port', port);
-  });
-}
-
-
-
+app.use(logRequest);
+app.use(express.static('public'));
+app.use(bodyParser.json());
 
 
-module.exports = {
-  start: serverStarting
+app.get('/test', functions.getTest);
+app.get('/test2', functions.getTest2);
+
+function logRequest(req, res, next) {
+  var parts = [
+    new Date(),
+    req.method,
+    req.originalUrl,
+  ];
+  next();
 }
