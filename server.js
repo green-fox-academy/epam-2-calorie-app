a83d56c13fce842794e8e3cb3416e208a501f00b
Daniel Bodonyi <bodonyid@gmail.com>
2016-02-02 13:33:17 +0100

    Add getTest2 and createQuery to server.js

--- server.js
+++ server.js
@@ -21,6 +21,20 @@ function getTest(req, res) {
   res.end();
 }
 
+app.get('/test2', getTest2);
+
+function getTest2(req, res) {
+  var url = req.originalUrl;
+  res.write(createQuery('get', url));
+  res.end();
+}
+
+function createQuery(requestType, url) {
+  if (requestType === 'get') {
+    return 'SELECT * FROM ' + url.substring(1, url.length);
+  }
+}
+
 function logRequest(req, res, next) {
   var parts = [
     new Date(),
