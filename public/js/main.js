e3095cc70b9dc0be9c42b84d5c4f76c60b41d4f1
Daniel Bodonyi <bodonyid@gmail.com>
2016-02-09 12:19:33 +0100

    Add main.js and index.html in Angular

--- public/js/main.js
+++ public/js/main.js
@@ -1,31 +1,9 @@
 'use strict';
 
+var app = angular.module('users', []);
 
-var url = window.location;
-var userDatabase = document.querySelector('.select-user-input');
-
-
-function getRequest(addition, callback) {
-  var myRequest = new XMLHttpRequest();
-  myRequest.open('GET',url + addition);
-  myRequest.setRequestHeader('Content-Type', 'application/json');
-  myRequest.send();
-  console.log(myRequest);
-  myRequest.onreadystatechange = function() {
-    if (myRequest.readyState === 4) {
-      callback(myRequest.response);
-      console.log(myRequest.response);
-    }
-  };
-}
-
-
-function userSelection(response) {
-  var usersArray = JSON.parse(response);
-  usersArray.forEach(function(oneuser) {
-    var listelement = document.createElement('option');
-    listelement.innerHTML = oneuser.user_name;
-    userDatabase.appendChild(listelement);
+app.controller('UserList', ['$scope', '$http', function($scope, $http) {
+  $http.get('/db/users').then(function(response) {
+    $scope.userData = response.data;
   });
-}
-getRequest('db/users', userSelection);
+}]);
