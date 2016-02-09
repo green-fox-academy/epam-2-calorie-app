fceda1a016488c2ac3ed2c287db45aaf852f442d
György Csadó <gyorgy.csado@gmail.com>
2016-02-09 10:16:16 +0100

    add angular js

--- items.js
+++ items.js
@@ -8,28 +8,18 @@ function getAll(psql, cb) {
 }
 
 // function add(params, cb) {
-//   var data = {};
-//   var wichUser = 'SELECT user_id FROM users WHERE name=?';
-//   psql.dbConnection(wichUser, params.body.user, function(err, result){
-//     if (params.body.date !== '') {
-//       data = {
-//         cons_name: params.body.name,
-//         cons_calories: params.body.calories,
-//         date: params.body.date,
-//         user_id: result[0].user_id
-//       };
-//     } else {
-//       data = {
-//         cons_name: params.body.name,
-//         cons_calories: params.body.calories,
-//         user_id: result[0].user_id
-//       };
-//     }
-//   });
-//   psql.dbConnection(data, cb);
+//   if (params.body.date !== '') {
+//     var post = 'INSERT INTO consumption (cons_name, cons_calories, date, user_id) VALUES (' +
+//     + params.body.cons_name +','+ params.body.cons_calories +','+ params.body.date +','+ params.body.user_id + ');';
+//     psql.dbConnection(post, cb);
+//   } else {
+//     var post = 'INSERT INTO consumption (cons_name, cons_calories, user_id) VALUES (' +
+//     + params.body.cons_name +','+ params.body.cons_calories +','+ params.body.user_id + ');';
+//     psql.dbConnection(post, cb);
+//   }
 // }
 
 module.exports = {
   getAll: getAll,
-  // add: add
+  add: add
 };
