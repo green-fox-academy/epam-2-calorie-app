7995616c1d56c3d219411e68636fa52d11441349
Edina Frank <frankedus@gmail.com>
2016-02-12 14:17:49 +0100

    testSpec bugfix

--- spec/testSpec.js
+++ spec/testSpec.js
@@ -1,14 +1,7 @@
 'use strict';
 
-var functions = require('../functions');
 var users = require('../users');
 var items = require('../items');
-
-describe('getTest2 returns string', function() {
-  it('createQuery returns string', function() {
-    expect(functions.createQuery('get', '/test2')).toBe('SELECT * FROM test2');
-  });
-});
 
 describe('test getAllUser', function() {
   it('is respons array', function(done) {
