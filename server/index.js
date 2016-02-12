638139a5e69f738af91dfea318c43452066510a4
György Csadó <gyorgy.csado@gmail.com>
2016-02-12 14:30:42 +0100

    cahnge index.js port access path

--- server/index.js
+++ server/index.js
@@ -2,11 +2,10 @@
 
 var connection = require('./database_connection');
 var server = require('./server.js')
-var config = require('./config.js');
+var port = require('./config.js').port;
 
 var app = server(connection);
 
-var port = parseInt(config.port);
 app.listen(port, function () {
   console.log('Listening on port', port);
 });
